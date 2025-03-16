2984ef822e31bb458eccaae780aa3ab63e66719c
ramya <ramyathammaiah16@gmail.com>
2025-03-16 20:07:41 +0530

    10th comments ,mathematical program

---
+++ src/ex_01_Java_Basic/Keywords_Identifier_program.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Keywords_Identifier_program {
+    public static void main(String[] args) {
+        System.out.println("keywords are the reserved words which has predefined meanings ");
+        System.out.println("keywords are start with lowercase ");
+        System.out.println("keywords are public, private, int, void etc");
+
+        System.out.println("Identifers are the name which is used to identity the programming elements ");
+        System.out.println("Identifiers are usually starts with alphabets, _, $ symbols. It can contain digits but not at begining on the word");
+    }
+}
