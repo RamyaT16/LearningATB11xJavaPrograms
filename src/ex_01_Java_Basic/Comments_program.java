ab2c4aaeb1637aba4edbc826281587387146f55a
ramya <ramyathammaiah16@gmail.com>
2025-03-16 19:51:56 +0530

    10th comments , println and print practise program

---
+++ src/ex_01_Java_Basic/Comments_program.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Comments_program {
+    public static void main(String[] args) {
+        System.out.println("Comments handson session");
+        //single line comment
+        //System.out.println("This is single line comments it will not display");
+        System.out.println("Multiple line comments");
+        /**
+        *System.out.println("Multiple line comments1");
+        *System.out.println("Multiple line comments2");
+        *System.out.println("Multiple line comments3");
+        **/
+
+
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Println_print_program.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Println_print_program {
+    public static void main(String[] args) {
+        System.out.println("output will print in different line");
+        System.out.println("Apple");
+        System.out.println("Betroot");
+        System.out.println("Carrot");
+        System.out.println("output will print in single line");
+        System.out.print("Apple+");
+        System.out.print("Betroot+");
+        System.out.print("Carrot juice id good for heart and skin");;
+    }
+}
