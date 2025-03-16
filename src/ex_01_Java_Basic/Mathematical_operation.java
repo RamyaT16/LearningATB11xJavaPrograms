ea530f3d021f312e46f777f16f834d49ca67b5cd
ramya <ramyathammaiah16@gmail.com>
2025-03-16 20:01:48 +0530

    10th comments ,mathematical program

---
+++ src/ex_01_Java_Basic/Mathematical_operation.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Mathematical_operation {
+    public static void main(String[] args) {
+        System.out.println("sum of 2 numbers: "  +(2+2));
+        System.out.println("sub of 2 numbers: "  +(2-2));
+        System.out.println("multiplication of 2 numbers: "  +(2*2));
+        System.out.println("Devision of 2 numbers: "  +(4/2));
+        System.out.println("Devision of 2 numbers: "  +(3/2));
+        System.out.println("Devision of 2 numbers: "  +(4/0));
+    }
+
+}
