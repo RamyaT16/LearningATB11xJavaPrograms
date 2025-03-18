af80a91f8dcbf501641f34e84fa1ba0c54819a8f
ramya <ramyathammaiah16@gmail.com>
2025-03-18 21:47:58 +0530

    11th table of 9 assignment

---
+++ src/ex_01_Java_Basic2/Table_of_9.java
@@ -0,0 +1,39 @@
+package ex_01_Java_Basic2;
+
+import java.sql.SQLOutput;
+
+public class Table_of_9 {
+    public static void main(String[] args) {
+        byte a = 9;
+        byte b = 1;
+        System.out.printf("%d * %d = %d",a,b,a*b);
+        System.out.println();
+        byte c = 2;
+        System.out.printf("%d * %d = %d",a,c,a*c);
+        System.out.println();
+        byte d = 3;
+        System.out.printf("%d * %d = %d" ,a,d,a*d);
+        System.out.println();
+        byte e = 4;
+        System.out.printf("%d * %d = %d" ,a,e,a*e);
+        System.out.println();
+        byte f = 5;
+        System.out.printf("%d * %d = %d" ,a,f,a*f);
+        System.out.println();
+        byte g = 6;
+        System.out.printf("%d * %d = %d" ,a,g,a*g);
+        System.out.println();
+        byte h = 7;
+        System.out.printf("%d * %d = %d" ,a,h,a*h);
+        System.out.println();
+        byte i = 8;
+        System.out.printf("%d * %d = %d" ,a,i,a*i);
+        System.out.println();
+        byte j = 9;
+        System.out.printf("%d * %d = %d" ,a,j,a*j);
+        System.out.println();
+        byte k = 10;
+        System.out.printf("%d * %d = %d" ,a,k,a*k);
+        System.out.println();
+    }
+}
